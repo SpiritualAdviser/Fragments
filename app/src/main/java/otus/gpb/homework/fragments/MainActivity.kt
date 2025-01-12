9dfeec5030e424649fa7c1cb0fbb2e1973cefa7b
SpiritualAdviser <pomaxam@narod.ru>
2025-01-12 13:03:19 +0300

    add buttons open activities A, B

--- app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
+++ app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
@@ -1,12 +1,28 @@
 package otus.gpb.homework.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            openActivityA.setOnClickListener {
+                startActivity(Intent(applicationContext, ActivityA::class.java))
+            }
+            openActivityB.setOnClickListener {
+                startActivity(Intent(applicationContext, ActivityB::class.java))
+            }
+        }
     }
 }
+
